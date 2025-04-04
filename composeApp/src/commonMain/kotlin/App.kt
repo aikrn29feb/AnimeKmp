e314872f6318fcce9c06c039c5fb7937c7a653cf
sk <kiran.s126@gmail.com>
2025-04-04 19:54:10 +0530

    Code cleanup after refactoring few classes

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,13 +1,2 @@
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import ui.CharactersScreen
-import viewmodel.CharactersViewModel
 
-@Composable
-fun App() {
-    val viewModel = CharactersViewModel()
-    
-    MaterialTheme {
-        CharactersScreen(viewModel)
-    }
-} 
+
